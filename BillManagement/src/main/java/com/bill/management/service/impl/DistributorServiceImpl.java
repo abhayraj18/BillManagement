74adb2663d382f4a079c318efbae987b96041776
Abhay Jain <abhay.jain@care.com>
2017-10-03 17:59:46 +0530

    Initial commits

--- BillManagement/src/main/java/com/bill/management/service/impl/DistributorServiceImpl.java
+++ BillManagement/src/main/java/com/bill/management/service/impl/DistributorServiceImpl.java
@@ -1,11 +1,13 @@
 package com.bill.management.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bill.management.repository.DistributorRepository;
 import com.bill.management.schema.Distributor;
 import com.bill.management.service.DistributorService;
 
+@Service
 public class DistributorServiceImpl implements DistributorService {
 
 	@Autowired
